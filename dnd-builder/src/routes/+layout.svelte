dfcc584363f7b0733213251061ca8cf61266b73a
Kyle Young <53121518+kyleyoung1121@users.noreply.github.com>
2025-08-02 19:05:47 -0400

    testing: add welcome page and class selection link

---
+++ dnd-builder/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<nav class="tabs space-x-4">
+	<a href="/" class="hover:underline">Welcome</a>
+	<a href="/class" class="hover:underline">Class</a>
+	<a href="/race" class="hover:underline">Race</a>
+	<a href="/abilities" class="hover:underline">Abilities</a>
+	<a href="/background" class="hover:underline">Background</a>
+	<a href="/equipment" class="hover:underline">Equipment</a>
+	<a href="/spells" class="hover:underline">Spells</a>
+	<a href="/export" class="hover:underline">Export</a>
+</nav>
+
+<main class="p-6">
+	<slot />
+</main>
