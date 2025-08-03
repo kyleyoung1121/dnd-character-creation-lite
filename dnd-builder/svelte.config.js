852f9731e2b0165f3d3fa7372bd11798edbaace3
Kyle Young <53121518+kyleyoung1121@users.noreply.github.com>
2025-08-03 13:02:07 -0400

    fixing svelte page deployment

--- dnd-builder/svelte.config.js
+++ dnd-builder/svelte.config.js
@@ -1,6 +1,14 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
+  kit: {
+		adapter: adapter(),
+		paths: {
+			base: '/dnd-character-creation-lite',
+		}
+	},
+  
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
